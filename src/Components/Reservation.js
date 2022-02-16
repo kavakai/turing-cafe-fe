ddda3b1b7a915d7d818613cf5694055079a628b2
Kai Kuller <kavakai@gmail.com>
2022-02-16 09:42:29 -0700

    Create fetch call and display reservation cards

--- src/Components/Reservation.js
+++ src/Components/Reservation.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './Reservation.css'
 
 const Reservation = ({ name, date, time, number }) => {
   return (
-    <div>
+    <div className='reservation-card'>
       <h2>{name}</h2>
       <h3>{date}</h3>
       <h3>{time}</h3>
--- src/Components/ReservationContainer.js
+++ src/Components/ReservationContainer.js
@@ -1,20 +1,22 @@
 import React from "react";
 import Reservation from "./Reservation";
+import './Reservation.css'
 
 
 const ReservationContainer = ({ reservations }) => {
-  reservations.map(reservation => {
+  let allReservations = reservations.map(reservation => {
     return <Reservation
       name={reservation.name}
       date={reservation.date}
       time={reservation.time}
       number={reservation.number}
+      key={reservation.id}
     />
   })
 
   return (
     <div>
-      <Reservation />
+      {allReservations}
     </div>
   )
 }
